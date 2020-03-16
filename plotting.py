085475e455e6fdb6a3327349806f813e63e24608
giuliobz <giulio.bazzanti@stud.unifi.it>
2020-03-16 20:02:30 +0100

    updates

--- plotting.py
+++ plotting.py
@@ -1,6 +1,8 @@
-
-
+import os
 import sys
+import glob
+import pandas as pd
+
 import matplotlib.pyplot as plt 
 import numpy as np 
 import csv 
@@ -10,15 +12,53 @@ import csv
 #    plt.fill_between(dims,means-stds,means+stds,alpha=.1)
 #    plt.show()
 
+def read_multiple_csv(path):
+
+        mean = []
+        std = []
+        axes = []
+
+        files = glob.glob(path + "/*.csv")
+
+        
+        for filename in files:
+            data_df = pd.read_csv(path + '/'+ filename, error_bad_lines=False, names=["mean", "std", "queries"])
+            mean.append(data_df["mean"].values[0])
+            std.append(data_df["std"].values[0])
+            axes.append(data_df["queries"].values[0])
+
+        return mean, std, axes
+
+def read_csv(path):
+
+    mean = []
+    std = []
+    axes = []
+
+    filename = os.listdir(path + '/')
+    data_df = pd.read_csv(path + '/' + filename[0], error_bad_lines=False, names=["mean", "std", "queries"])
+
+    for i in range(data_df["mean"].values):
+        mean.append(data_df["mean"].values[i])
+        std.append(data_df["std"].values[i])
+        axes.append(data_df["queries"].values[i])
+
+
 if __name__ == "__main__":
 
     path = sys.argv[1]
     test_method = sys.argv[2]
 
-    print(path)
-    print(test_method)
+    mean = []
+    std = []
+    axes = []
 
-    
+    if test_method == "pss":
+        mean, std, axes = read_multiple_csv(path)
+
+    else:
+        mean, std, axes = read_csv(path)
+
 
     #means = []
     #stds = []
