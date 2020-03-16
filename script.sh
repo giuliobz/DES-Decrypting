fcfce72de7765899221f65cccfd7522e5963bb3b
giuliobz <giulio.bazzanti@stud.unifi.it>
2020-03-16 15:16:51 +0100

    updates

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+maxThreads=12
+dictionary="Dictionaries/Dictionary_data.txt"
+maxIteration=10
+numberPss=5
+method="Callable"
+for i in {1..10}
+do
+  ans=$((numberPss * i))
+
+  echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo "Searching  $ans passwords  "
+  echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo ""
+
+  java -jar /home/bazza/Scrivania/DES-Decrypting/out/artifacts/DES_Decrypting_jar/DES-Decrypting.jar $maxThreads $dictionary $maxIteration $ans $method
+
+done
+
+
+
